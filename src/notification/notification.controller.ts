91bc0abd17cedefd307416dc14675f1a77e4607b
wellington <wellingtonfds@gmail.com>
2024-04-04 22:26:55 -0300

    init swagger

---
+++ src/notification/notification.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('notification')
+export class NotificationController {
+
+
+}
--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WhatsAppModule } from './whats-app/whats-app.module';
+import { NotificationController } from './notification.controller';
 
 @Module({
-    imports: [WhatsAppModule]
+    imports: [WhatsAppModule],
+    controllers: [NotificationController]
 })
 export class NotificationModule { }
