c57ef15627e0867ed108f45fbe33c220c5729178
wellington <wellingtonfds@gmail.com>
2024-04-18 22:37:32 -0300

    create auth sicoob

--- src/payment/payment.config.ts
+++ src/payment/payment.config.ts
@@ -5,7 +5,8 @@ export default () => ({
         clientId: process.env.SICOOB_CLIENT_ID,
         sandBox: process.env.SICOOB_SANDBOX,
         apiUrl: process.env.SICOOB_API_URL,
-        certificate: fs.readFileSync('./private_key.pem', 'utf8'),
+        cert: fs.readFileSync('./private_key.pem', 'utf8'),
+        key: fs.readFileSync('keyfile.key', 'utf-8'),
     }
 
 })
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,22 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
+import https from 'https';
 import { Payment } from './types/register-payment-pix';
-
 @Injectable()
 export class PaymentService {
 
     private apiUrl: string
 
-    public constructor(config: ConfigService) { }
+    public constructor(private config: ConfigService) { }
 
-    public getToken() {
+    public async getToken() {
+        const client_id = this.config.get('sicoob.clientId')
+        const sendBox = this.config.get('sicoob.sendbox')
+        if (client_id && sendBox) {
+            return {
+                client_id,
+                Authorization: `Bearer  ${sendBox}`
+            }
+        }
+        const agent = new https.Agent({
+            cert: this.config.get('sicoob.cert'),
+            key: this.config.get('sicoob.key'),
+            keepAlive: true,
+            rejectUnauthorized: false
+        })
+        // enviar token .pem
+        const config = {
+            agent,
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: `${this.config.get('sicoob.apiUrl')}/auth/realms/cooperado/protocol/openid-connect/token`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+                grant_type: 'client_credentials',
+                client_id,
+                scope: 'cob.read cob.write cobv.write cobv.read lotecobv.write lotecobv.read pix.write pix.read webhook.read webhook.write payloadlocation.write payloadlocation.read'
+            }
+        };
 
+        const response = await axios(config)
+
+        const token = response.data
+        return {
+            client_id,
+            Authorization: `Bearer  ${token}`
+        }
     }
 
     public registerPix(payment: Payment) {
 
-        axios.post(this.apiUrl, payment)
+        axios.post(this.apiUrl, payment, {
+
+        })
 
     }
 
