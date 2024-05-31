669631ae6fc3c7af9c751e7d80f491b11f5b6174
wellington Fernandes <wellingtonfds@gmail.com>
2024-05-31 14:47:37 -0300

    fix deploy

--- src/payment/payment.config.ts
+++ src/payment/payment.config.ts
@@ -1,13 +1,14 @@
 
-import fs from 'fs'
 
 export default () => ({
     sicoob: {
         clientId: process.env.SICOOB_CLIENT_ID,
         sandBox: process.env.SICOOB_SANDBOX,
         apiUrl: process.env.SICOOB_API_URL,
-        cert: fs.readFileSync('./certificado.crt'),
-        key: fs.readFileSync('./public.key')
+        cert: '',
+        // cert: fs.readFileSync('./certificado.crt'),
+        key: ''
+        // key: fs.readFileSync('./public.key')
     }
 
 })
