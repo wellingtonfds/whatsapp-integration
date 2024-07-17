755be8afc5864ae35072b3aec76efa6f45f079df
Pedro Liberato <phliberato@engetower.com.br>
2024-07-16 22:03:24 -0300

    Cooperativa de testes

--- src/granatum/types/contaTipo.ts
+++ src/granatum/types/contaTipo.ts
@@ -1,7 +1,8 @@
 enum ContaTipo {
     Caixa = '85351',
     FluxoDeCaixa = '85342',
-    Cooperativa = '111126'
+    //Cooperativa = '111126', //Cooperativa produção
+    Cooperativa = '114381' //Cooperativa para testes
 }
 
 export default ContaTipo;
